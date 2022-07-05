74312bb28683b38c0bc9ca31cc522b5c0a31daba
mwrina <marinaarosa27@gmail.com>
2022-07-05 18:03:15 -0300

    first commit

--- COMEBEAR/PHP/pedido.php
+++ COMEBEAR/PHP/pedido.php
@@ -2,27 +2,6 @@
 
 include_once('connect.php');
 
-//GET 
-
-$nome = '';
-
-if(!empty ($_POST['nome'])){
-    $nome = $_POST['nome'];
-}
-
-if(!empty ($_POST['nome_pedido'])){
-    $nome_pedido = $_POST['nome_pedido'];
-}
-
-$sql1 = "SELECT * FROM usuario WHERE nome_usuario = '$nome'";
-$result1 = mysqli_query($conn, $sql1);
-
-if(mysqli_num_rows($result1) > 0){
-    while($row1 = mysqli_fetch_assoc($result1)) {
-        $cpf_usu = $row1["cpf_usuario"];
-    }
-}
-
 //GET NOME_PEDIDO
 
 $strhtml = '<div id="nome_pedido">
@@ -48,7 +27,30 @@ if( $col ){
 
 $nome_pedido = $tags['h3'];
 
+//GET GERAL
 
+$nome = '';
 
+if(!empty ($_POST['nome'])){
+    $nome = $_POST['nome'];
+}
+
+$sql1 = "SELECT * FROM usuario WHERE nome_usuario = '$nome'";
+$result1 = mysqli_query($conn, $sql1);
+
+if(mysqli_num_rows($result1) > 0){
+    while($row1 = mysqli_fetch_assoc($result1)) {
+        $cpf_usu = $row1["cpf_usuario"];
+    }
+}
+
+$sql2 = "SELECT * FROM produto WHERE nome_produto = '$nome_produto'";
+$result2 = mysqli_query($conn, $sql2);
+
+if(mysqli_num_rows($result2) > 0){
+    while($row2 = mysqli_fetch_assoc($result2)) {
+        $cpf_usu = $row2["id_estab"];
+    }
+}
 
 ?>
